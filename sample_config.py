380ed4cf95a2069418df6aa5f0b67f1400450df1
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-01-07 21:19:37 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -7,7 +7,7 @@ class Config(object):
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
     
     #log channel
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNELS = int(os.environ.get("LOG_CHANNEL", ""))
     
     #update channel
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
