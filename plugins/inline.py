95429b6114d6d9eabd9560d3326df4239590cbf5
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-01-07 19:58:23 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -17,7 +17,7 @@ async def answer(bot, query):
     if not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='Min hmet la channel join phawt rawh',
                            switch_pm_parameter="subscribe")
         return
 
@@ -91,7 +91,10 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat="")
+            InlineKeyboardButton('Zawn lehna', switch_inline_query_current_chat="")
+        ]
+        [
+            InlineKeyboardButton('Bot', switch_inline_query_current_chat="")
         ]
         ]
     return InlineKeyboardMarkup(buttons)
