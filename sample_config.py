6cdaac2132703f5f5b97e46283cf1e016d577132
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-01-07 21:17:52 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,5 +6,8 @@ class Config(object):
     # database uri (mongodb)
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
     
+    #log channel
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    
     #update channel
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
