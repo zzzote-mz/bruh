6ba0f916fb98e4712776bdd2e16256437c43b031
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-01-07 20:22:40 +0530

    Create z.py

---
+++ plugins/z.py
@@ -0,0 +1,17 @@
+import asyncio
+from pyrogram import Client, filters
+from plugins.fsub import ForceSub
+
+
+
+@Client.on_message(filters.private & filters.text)
+async def nor(client, message):
+    await AddUser(client, message)
+    FSub = await ForceSub(client, message)
+    if FSub == 400:
+        return
+    await client.send_message(
+        message.chat.id,
+        text="**i thil zawn hi ka ka hmuzo miahlo mai, ka database ah a awmlo emaw i search dan a fuh tawklo ani maithei ani.**",
+        reply_to_message_id=message.message_id
+    )
