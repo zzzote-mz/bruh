b8d1ed6c0e5d7d15099c9793597d02cc659a7afb
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-01-08 18:00:05 +0530

    Update A.py

--- plugins/A.py
+++ plugins/A.py
@@ -6,7 +6,7 @@ from info import ADMINS
 @Client.on_message(filters.command('req'))
 async def report(client, message):
   rsr = message.text.split(None, 1)[1]
-  await client.send_message(chat_id=ADMINS, text=f"<b>⭕️Request⭕️\n\n🧿 Name: {message.from_user.mention}\n🧿 User ID:</b> <code>{message.chat.id}</code>\n<b>🧿 Req:</b> {rsr}")
+  await client.send_message(chat_id=ADMINS, text=f"<b>⭕️Request⭕️\n\n🧿 Name: {message.from_user.mention}\n🧿 User ID:</b> <code>{message.chat.id}</code>\n<b>🧿 Req:</b> rsr")
   await client.send_message(message.chat.id, text="<b>✅ I request chu min siamtu hnen ah thawn ani e.</b>", reply_to_message_id=message.message_id)
 
         
