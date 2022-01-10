885f5d43be296a003c1ee0d4604771b9d23bad88
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-01-10 09:27:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -172,7 +172,7 @@ async def start(client, message):
             return
         except:
             pass
-        return await message.reply('No such file exist.')
+        return await message.reply('File hmuh anilo.')
     files = files_[0]
     title = files.file_name
     size=get_size(files.file_size)
