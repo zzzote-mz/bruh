88658cb29958e0544fa21983ad60fe0ceb2106db
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-01-10 09:27:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -351,7 +351,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
         if not files_:
-            return await query.answer('No such file exist.')
+            return await query.answer('File hmuh anilo.')
         files = files_[0]
         title = files.file_name
         size=get_size(files.file_size)
