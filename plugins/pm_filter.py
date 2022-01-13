21b54667244e063bb735ee1eff5e16d3fb7e3f66
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-01-13 22:13:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -58,7 +58,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("itih tur anilo", show_alert=True)
     try:
         offset = int(offset)
     except:
