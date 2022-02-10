7d322a23bb711c3553b27a1357c9ead78b773d32
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-02-10 11:27:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,7 +43,7 @@ async def start(client, message):
             InlineKeyboardButton('Helpline', url='https://t.me/helptereuhte'),
             InlineKeyboardButton('Channel', url='https://t.me/rsrbots')
             ],[
-            InlineKeyboardButton('Developer', url='https://t.me/rsrmusic'),
+            InlineKeyboardButton('Developer', user_id='1060318977'),
             InlineKeyboardButton('About', callback_data='about')
             ],[
             InlineKeyboardButton('Share', url='https://t.me/share/url?url=Mizo%20%E1%B9%ADawnga%20lehlin%20movie%20%E1%B9%ADhenkhat%20zawn%20na%20leh%20en%20na%20hi%20i%20hman%20ve%20duh%20chuan%20a%20hnuaia%20Bot%20tia%20ka%20kawh%20na%20zawn%20a%20username%20khu%20hmet%20la%20i%20hmang%20thei%20ang%2C%20a%20free%20vek%20in.%0A%0ABot%20%F0%9F%91%89%20%40mzmvbot')
@@ -86,7 +86,7 @@ async def start(client, message):
             InlineKeyboardButton('Helpline', url='https://t.me/helptereuhte'),
             InlineKeyboardButton('Channel', url='https://t.me/rsrbots')
             ],[
-            InlineKeyboardButton('Developer', url='https://t.me/rsrmusic'),
+            InlineKeyboardButton('Developer', user_id='1060318977'),
             InlineKeyboardButton('About', callback_data='about')
             ],[
             InlineKeyboardButton('Share', url='https://t.me/share/url?url=Mizo%20%E1%B9%ADawnga%20lehlin%20movie%20%E1%B9%ADhenkhat%20zawn%20na%20leh%20en%20na%20hi%20i%20hman%20ve%20duh%20chuan%20a%20hnuaia%20Bot%20tia%20ka%20kawh%20na%20zawn%20a%20username%20khu%20hmet%20la%20i%20hmang%20thei%20ang%2C%20a%20free%20vek%20in.%0A%0ABot%20%F0%9F%91%89%20%40mzmvbot')
