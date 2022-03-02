2b90aa67661ee46d3cbaae24e80e109b0a664ce4
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-03-02 11:02:19 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -11,8 +11,8 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = []
+CHANNELS = []
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
