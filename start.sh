ae0764cc7e3986c0fead152653fe14f1e6bad031
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-17 21:31:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b main https://github.com/RSR-TG-Info/mizomv1.git /mizomv1
-cd /mizomv1
+git clone -b main https://github.com/RSR-TG-Info/TereuhteBackup.git /TereuhteBackup
+cd /TereuhteBackup
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
