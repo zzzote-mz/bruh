c54dc0679f9ab5f36a316c9e0c5e591b3570dfc5
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-19 21:43:40 +0530

    Create banning.py

---
+++ plugins/banning.py
@@ -0,0 +1,47 @@
+# ©️2022 RSR
+from pyrogram import Client, filters
+
+
+
+@Client.on_message(filters.command("ban", prefixes=["/", "!"]))
+async def ban(client, message):
+    if message.reply_to_message:
+        uid = message.reply_to_message.from_user.id
+        umen = message.reply_to_message.from_user.mention
+        await client.ban_chat_member(message.chat.id, user_id=uid)
+        await client.send_message(message.chat.id, text=f"{umen} hi Ban ani e.", reply_to_message_id=message.message_id)
+        return
+    else:
+        umens = message.reply_to_message.from_user.mention
+        idu = message.text.split(None, 1)[1]
+        await client.ban_chat_member(message.chat.id, user_id=idu)
+        await client.send_message(message.chat.id, text=f"{umens} hi Ban ani e.", reply_to_message_id=message.message_id)
+        return
+      
+      
+@Client.on_message(filters.command("unban", prefixes=["/", "!"]))
+async def unban(client, message):
+    if message.reply_to_message:
+        uud = message.reply_to_message.from_user.id
+        umun = message.reply_to_message.from_user.mention
+        await client.unban_chat_member(message.chat.id, user_id=uud)
+        await client.send_message(message.chat.id, text=f"{umun} hi Ban anihna hlih sak ani e.", reply_to_message_id=message.message_id)
+        return
+    else:
+        umuns = message.reply_to_message.from_user.mention
+        idus = message.text.split(None, 1)[1]
+        await client.unban_chat_member(message.chat.id, user_id=idus)
+        await client.send_message(message.chat.id, text=f"{umuns} hi Ban anihna hlih sak ani e.", reply_to_message_id=message.message_id)
+        return
+      
+      
+      
+@Client.on_message(filters.command("removeme", prefixes=["/", "!"]))
+async def selfb(client, message):
+    si = message.from_user.id
+    await client.ban_chat_member(message.chat.id, user_id=si)
+    await client.send_message(message.chat.id, text=f"Aw le, remove ini e, Bye...", reply_to_message_id=message.message_id)
+    return      
+      
+      
+      
