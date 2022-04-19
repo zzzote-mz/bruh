febe8e0f88b1eb579276d4424d7f754e5d27e642
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-19 17:33:48 +0530

    Update inpuihtawnna.py

--- plugins/inpuihtawnna.py
+++ plugins/inpuihtawnna.py
@@ -36,4 +36,4 @@ rsrke = InlineKeyboardMarkup(
 @Client.on_message(filters.chat(-1001465429519) & filters.new_chat_members)
 async def inpuihtawnna(client, message):
     for rsr in message.new_chat_members:
-          await client.send_message(-1001465429519, text=f"Hello {rsr.mention}\n\n**{message.chat.title}** group ah kan lo lawm a che. He group hi Telegram ani emaw thildang pawh nise i hriatloh i zawhna hmun tur ani e. Tin, i hriat zawng an lo zawh pawn tim miahlo in ilo chhang thei ang😊,Rules khu chhiar tur aw.", reply_markup=rsrke, reply_to_message_id=message.message_id)
+          await client.send_message(-1001465429519, text=f"Hello {rsr.mention}\n\n**{message.chat.title}** group ah kan lo lawm a che. He group hi Telegram ani emaw thildang pawh nise i hriatloh i zawhna hmun tur ani e. Tin, i hriat zawng an lo zawh pawn tim miahlo in ilo chhang thei ang😊, Rules khu chhiar tur aw.", reply_markup=rsrke, reply_to_message_id=message.message_id)
