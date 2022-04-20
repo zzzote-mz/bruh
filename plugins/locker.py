ca8b1d28e23d61b720d8d38a0e65e6f67a423bd9
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-20 22:01:18 +0530

    Update locker.py

--- plugins/locker.py
+++ plugins/locker.py
@@ -15,7 +15,7 @@ async def close(client, message):
             return
     else:
         await client.set_chat_permissions(chat_id=message.chat.id, permissions=ChatPermissions())
-        await client.send_message(message.chat.id, text="**Group ah hian member tan message thawn theihloh tura siam ani e.**", reply_to_message_id=message.message_id)
+        await client.send_message(message.chat.id, text="**Group ah hian member tan message thawn theih loh tura siam ani e.**", reply_to_message_id=message.message_id)
         return
       
 @Client.on_message(filters.command("open", prefixes=["/", "!"]))
@@ -36,9 +36,10 @@ async def open(client, message):
                 can_send_media_messages=True,
                 can_send_other_messages=True,
                 can_invite_users=True,
-                can_add_web_page_previews=True
+                can_add_web_page_previews=True,
+                can_send_polls=True
             )
         )
-        await client.send_message(message.chat.id, text="**Group ah hian member tan paw'n message thawn thei tura siam ani e.**", reply_to_message_id=message.message_id)
+        await client.send_message(message.chat.id, text="**Group ah hian member tan paw'n message thawn theih tura siam ani e.**", reply_to_message_id=message.message_id)
         return     
       
