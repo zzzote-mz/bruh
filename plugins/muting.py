f9681e8382149b4083c892fa41836784b23580ab
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-20 21:53:15 +0530

    Update muting.py

--- plugins/muting.py
+++ plugins/muting.py
@@ -20,14 +20,14 @@ async def mute(client, message):
     elif message.reply_to_message:
         uid = message.reply_to_message.from_user.id
         umen = message.reply_to_message.from_user.mention
-        await client.restrict_chat_member(message.chat.id, user_id=uid, permissions=ChatPermissions())
+        await client.restrict_chat_member(chat_id=message.chat.id, user_id=uid, permissions=ChatPermissions())
         await client.send_message(message.chat.id, text=f"{umen} hi Mute ani e.", reply_to_message_id=message.message_id)
         return
     else:
         idu = message.text.split(None, 1)[1]
         hmm = await client.get_users(idu)
         umens = hmm.mention
-        await client.restrict_chat_member(message.chat.id, user_id=idu, permissions=ChatPermissions())
+        await client.restrict_chat_member(chat_id=message.chat.id, user_id=idu, permissions=ChatPermissions())
         await client.send_message(message.chat.id, text=f"{umens} hi Mute ani e.", reply_to_message_id=message.message_id)
         return
       
@@ -48,15 +48,15 @@ async def unmute(client, message):
     elif message.reply_to_message:
         uud = message.reply_to_message.from_user.id
         umun = message.reply_to_message.from_user.mention
-        await client.unban_chat_member(message.chat.id, user_id=uud)
-        await client.send_message(message.chat.id, text=f"{umun} hi Mute anihna hlih sak ani e.", reply_to_message_id=message.message_id)
+        await client.unban_chat_member(chat_id=message.chat.id, user_id=uud)
+        await client.send_message(chat_id=message.chat.id, text=f"{umun} hi Mute anihna hlih sak ani e.", reply_to_message_id=message.message_id)
         return
     else:
         idus = message.text.split(None, 1)[1]
         haa = await client.get_users(idus)
         umuns = haa.mention
-        await client.unban_chat_member(message.chat.id, user_id=idus)
-        await client.send_message(message.chat.id, text=f"{umuns} hi Mute anihna hlih sak ani e.", reply_to_message_id=message.message_id)
+        await client.unban_chat_member(chat_id=message.chat.id, user_id=idus)
+        await client.send_message(chat_id=message.chat.id, text=f"{umuns} hi Mute anihna hlih sak ani e.", reply_to_message_id=message.message_id)
         return
       
       
