738c10aa5db43633cab0e5b3201471f8812dd42c
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-21 18:47:59 +0530

    Update pin.py

--- plugins/pin.py
+++ plugins/pin.py
@@ -2,7 +2,7 @@
 from pyrogram.errors import RightForbidden
 from pyrogram import Client, filters
 from Tereuhte.tetakte.helper import admins_only
-
+from pyrogram.filters import regex
 
 
 @Client.on_message(filters.command("pin", prefixes=["/", "!"]))
@@ -70,4 +70,50 @@ async def unpin_message(client, message):
     except RightForbidden:
         await m.reply_text("**Hetah message pin emaw unpin theihna permission ka neilo.**")
     
+    return
+                                     
+                                     
+                                     
+@Client.on_message(filters.command("unpinall", prefixes=["/", "!"]))
+@admins_only
+async def unpinall_message(client, message):
+    await message.reply_text(
+        "**Group a message pin zawng zawng hi unpin vek i duh tih i chiang maw?**",
+        reply_markup=ikb([[("Yes", "unpin_all_in_this_chat"), ("No", "close_admin")]]),
+    )
+    return
+
+
+@Alita.on_callback_query(regex("^unpin_all_in_this_chat$"))
+async def unpinall_calllback(c: Alita, q: CallbackQuery):
+    user_id = q.from_user.id
+    user_status = (await q.message.chat.get_member(user_id)).status
+    if user_status not in {"creator", "administrator"}:
+        await q.answer(
+            "You're not even an admin, don't try this explosive shit!",
+            show_alert=True,
+        )
+        return
+    if user_status != "creator":
+        await q.answer(
+            "You're just an admin, not owner\nStay in your limits!",
+            show_alert=True,
+        )
+        return
+    try:
+        await c.unpin_all_chat_messages(q.message.chat.id)
+        LOGGER.info(f"{q.from_user.id} unpinned all messages in {q.message.chat.id}")
+        await q.message.edit_text(tlang(q, "pin.unpinned_all_msg"))
+    except ChatAdminRequired:
+        await q.message.edit_text(tlang(q, "admin.notadmin"))
+    except RightForbidden:
+        await q.message.edit_text(tlang(q, "pin.no_rights_unpin"))
+    except RPCError as ef:
+        await q.message.edit_text(
+            (tlang(q, "general.some_error")).format(
+                SUPPORT_GROUP=SUPPORT_GROUP,
+                ef=ef,
+            ),
+        )
+        LOGGER.error(ef)
     return
