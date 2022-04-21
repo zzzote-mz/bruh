05c27bee4a39b80f4f4e766490f76c8744e53d5d
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-22 00:04:23 +0530

    Update group.py

--- plugins/group.py
+++ plugins/group.py
@@ -7,14 +7,7 @@ from Tereuhte.tetakte.helper import admins_only
 @Client.on_message(filters.command("setgtitle", prefixes=["/", "!"]))
 @admins_only
 async def setgtitle(client, message):
-    if message.chat.type == "private":
-            await client.send_message(
-                message.chat.id,
-                text="**Hei chu group ah chauh a hman theih.**",
-                reply_to_message_id=message.message_id
-            )
-            return
-    elif len(message.command) < 2:
+    if len(message.command) < 2:
         return await message.reply_text("A hming tur dah tel rawh.")
     old_title = message.chat.title
     new_title = message.text.split(None, 1)[1]
@@ -30,13 +23,6 @@ async def setgtitle(client, message):
 @Client.on_message(filters.command("setgpic", prefixes=["/", "!"]))
 @admins_only
 async def setgpic(client, message):
-    if message.chat.type == "private":
-            await client.send_message(
-                message.chat.id,
-                text="**Hei chu group ah chauh a hman theih.**",
-                reply_to_message_id=message.message_id
-            )
-            return
     reply = message.reply_to_message
 
     if not reply:
@@ -65,16 +51,9 @@ async def setgpic(client, message):
 @Client.on_message(filters.command("delgpic", prefixes=["/", "!"]))
 @admins_only   
 async def delgpic(client, message):
-    if message.chat.type == "private":
-            await client.send_message(
-                message.chat.id,
-                text="**Hei chu group ah chauh a hman theih.**",
-                reply_to_message_id=message.message_id
-            )
-            return
-            await client.delete_chat_photo(chat_id=message.chat.id)
-            await message.reply_text("Group icon delete ani e.")
-            return
+    await client.delete_chat_photo(chat_id=message.chat.id)
+    await message.reply_text("Group icon delete ani e.")
+    return
     
     
     
