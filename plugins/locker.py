5f1e2bdb1df622c85ec5df8464f6caa2b541c1e1
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-22 00:06:59 +0530

    Update locker.py

--- plugins/locker.py
+++ plugins/locker.py
@@ -6,40 +6,24 @@ from pyrogram.types import ChatPermissions
 @Client.on_message(filters.command("close", prefixes=["/", "!"]))
 @admins_only
 async def close(client, message):
-    if message.chat.type == "private":
-            await client.send_message(
-                message.chat.id,
-                text="**Hei chu group ah chauh a hman theih.**",
-                reply_to_message_id=message.message_id
-            )
-            return
-    else:
-        await client.set_chat_permissions(chat_id=message.chat.id, permissions=ChatPermissions())
-        await client.send_message(message.chat.id, text="**Group ah hian member tan message thawn theih loh tura siam ani e.**", reply_to_message_id=message.message_id)
-        return
+    await client.set_chat_permissions(chat_id=message.chat.id, permissions=ChatPermissions())
+    await client.send_message(message.chat.id, text="**Group ah hian member tan message thawn theih loh tura siam ani e.**", reply_to_message_id=message.message_id)
+    return
       
 @Client.on_message(filters.command("open", prefixes=["/", "!"]))
 @admins_only
 async def open(client, message):
-    if message.chat.type == "private":
-            await client.send_message(
-                message.chat.id,
-                text="**Hei chu group ah chauh a hman theih.**",
-                reply_to_message_id=message.message_id
-            )
-            return
-    else:
-        await client.set_chat_permissions(
-            chat_id=message.chat.id,
-            permissions=ChatPermissions(
-                can_send_messages=True,
-                can_send_media_messages=True,
-                can_send_other_messages=True,
-                can_invite_users=True,
-                can_add_web_page_previews=True,
-                can_send_polls=True
-            )
+    await client.set_chat_permissions(
+        chat_id=message.chat.id,
+        permissions=ChatPermissions(
+            can_send_messages=True,
+            can_send_media_messages=True,
+            can_send_other_messages=True,
+            can_invite_users=True,
+            can_add_web_page_previews=True,
+            can_send_polls=True
         )
-        await client.send_message(message.chat.id, text="**Group ah hian member tan paw'n message thawn theih tura siam ani e.**", reply_to_message_id=message.message_id)
-        return     
+    )
+    await client.send_message(message.chat.id, text="**Group ah hian member tan paw'n message thawn theih tura siam ani e.**", reply_to_message_id=message.message_id)
+    return     
       
