63a988b11fe2013ad5e34b840fe75606b84fa8bd
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-21 17:50:48 +0530

    Update pin.py

--- plugins/pin.py
+++ plugins/pin.py
@@ -3,38 +3,33 @@
 
 
 
-
-async def pin_message(_, m: Message):
-    pin_args = m.text.split(None, 1)
-    if m.reply_to_message:
+@Client.on_message(filters.command("pin", prefixes=["/", "!"]))
+@admins_only
+async def pin_message(client, message):
+    pin_args = message.text.split(None, 1)
+    if message.reply_to_message:
         try:
             disable_notification = True
 
-            if len(pin_args) >= 2 and pin_args[1] in ["alert", "notify", "loud"]:
+            if len(pin_args) >= 2 and pin_args[1] in ["loud"]:
                 disable_notification = False
 
-            await m.reply_to_message.pin(
+            await message.reply_to_message.pin(
                 disable_notification=disable_notification,
             )
-            LOGGER.info(
-                f"{m.from_user.id} pinned msgid-{m.reply_to_message.message_id} in {m.chat.id}",
-            )
 
-            if m.chat.username:
-                # If chat has a username, use this format
-                link_chat_id = m.chat.username
-                message_link = (
-                    f"https://t.me/{link_chat_id}/{m.reply_to_message.message_id}"
+            if message.chat.username:
+                link_chat_id = message.chat.username
+                hmm = (
+                    f"https://t.me/{link_chat_id}/{message.reply_to_message.message_id}"
                 )
-            elif (str(m.chat.id)).startswith("-100"):
-                # If chat does not have a username, use this
-                link_chat_id = (str(m.chat.id)).replace("-100", "")
-                message_link = (
-                    f"https://t.me/c/{link_chat_id}/{m.reply_to_message.message_id}"
+            elif (str(message.chat.id)).startswith("-100"):
+                link_chat_id = (str(message.chat.id)).replace("-100", "")
+                hmm = (
+                    f"https://t.me/c/{link_chat_id}/{message.reply_to_message.message_id}"
                 )
-            await m.reply_text(
-                tlang(m, "pin.pinned_msg").format(message_link=message_link),
-                disable_web_page_preview=True,
+            await message.reply_text(
+                "**He message hi ka pin e.**",
             )
 
         except ChatAdminRequired:
