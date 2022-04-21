b98bdb63c475aba703e39c03c8b20221af99464c
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-21 23:45:14 +0530

    Update private.py

--- plugins/private.py
+++ plugins/private.py
@@ -35,6 +35,37 @@ async def remove(client, message):
     return
 
 
+@Client.on_message(filters.command("setgtitle", prefixes=["/", "!"]) & filters.private)
+async def setgtitle(client, message):
+    await client.send_message(
+        message.chat.id,
+        text="**Hei chu group ah chauh a hman theih.**",
+        reply_to_message_id=message.message_id
+    )
+    return
+
+
+@Client.on_message(filters.command("setgpic", prefixes=["/", "!"]) & filters.private)
+async def setgpic(client, message):
+    await client.send_message(
+        message.chat.id,
+        text="**Hei chu group ah chauh a hman theih.**",
+        reply_to_message_id=message.message_id
+    )
+    return
+
+
+
+@Client.on_message(filters.command("delgpic", prefixes=["/", "!"]) & filters.private)
+async def delgpic(client, message):
+    await client.send_message(
+        message.chat.id,
+        text="**Hei chu group ah chauh a hman theih.**",
+        reply_to_message_id=message.message_id
+    )
+    return
+
+
 @Client.on_message(filters.command("removeme", prefixes=["/", "!"]) & filters.private)
 async def removeme(client, message):
     await client.send_message(
