5c033dde5e73b85a6745abc7ccc7e26cda7b2922
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-21 23:43:27 +0530

    Update private.py

--- plugins/private.py
+++ plugins/private.py
@@ -6,4 +6,40 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("ban", prefixes=["/", "!"]) & filters.private)
 async def ban(client, message):
-    await client.send_message(message.chat.id, text="**Hei chu group ah chauh a hman theih.**",
+    await client.send_message(
+        message.chat.id,
+        text="**Hei chu group ah chauh a hman theih.**",
+        reply_to_message_id=message.message_id
+    )
+    return
+
+
+
+@Client.on_message(filters.command("unban", prefixes=["/", "!"]) & filters.private)
+async def unban(client, message):
+    await client.send_message(
+        message.chat.id,
+        text="**Hei chu group ah chauh a hman theih.**",
+        reply_to_message_id=message.message_id
+    )
+    return
+
+
+@Client.on_message(filters.command("remove", prefixes=["/", "!"]) & filters.private)
+async def remove(client, message):
+    await client.send_message(
+        message.chat.id,
+        text="**Hei chu group ah chauh a hman theih.**",
+        reply_to_message_id=message.message_id
+    )
+    return
+
+
+@Client.on_message(filters.command("removeme", prefixes=["/", "!"]) & filters.private)
+async def removeme(client, message):
+    await client.send_message(
+        message.chat.id,
+        text="**Hei chu group ah chauh a hman theih.**",
+        reply_to_message_id=message.message_id
+    )
+    return
