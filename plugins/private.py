e9bcff8e012050844e79d3944a53d988b75be7be
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-21 23:41:17 +0530

    Update private.py

--- plugins/private.py
+++ plugins/private.py
@@ -4,4 +4,6 @@ from pyrogram import Client, filters
 
 
 
-@Client.on_message(filters.command("mute", prefixes=["/", "!"]) & filters.private)
+@Client.on_message(filters.command("ban", prefixes=["/", "!"]) & filters.private)
+async def ban(client, message):
+    await client.send_message(message.chat.id, text="**Hei chu group ah chauh a hman theih.**",
