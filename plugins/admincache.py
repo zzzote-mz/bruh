d435d7e4d14eb3b9305cd5e38b7d6437284dd216
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-22 22:35:23 +0530

    Update admincache.py

--- plugins/admincache.py
+++ plugins/admincache.py
@@ -17,47 +17,46 @@ async def adminlist(client, message):
             "**Hei chu group ah chauh a hman theih.**",
         )
     try:
-        try:
-            admin_list = ADMIN_CACHE[message.chat.id]
-            note = "Cache value."
-        except KeyError:
-            admin_list = await admin_cache_reload(message, "adminlist")
-            note = "Up to date value."
+        admin_list = ADMIN_CACHE[message.chat.id]
+        note = "Cache value."
+    except KeyError:
+        admin_list = await admin_cache_reload(message, "adminlist")
+        note = "Up to date value."
 
-        adminstr = f"**{chat_title}** a Admin te chu:".format(
-            chat_title=m.chat.title,
-        ) + "\n\n"
+    adminstr = f"**{chat_title}** a Admin te chu:".format(
+        chat_title=m.chat.title,
+    ) + "\n\n"
 
-        bot_admins = [i for i in admin_list if (i[1].lower()).endswith("bot")]
-        user_admins = [i for i in admin_list if not (i[1].lower()).endswith("bot")]
+    bot_admins = [i for i in admin_list if (i[1].lower()).endswith("bot")]
+    user_admins = [i for i in admin_list if not (i[1].lower()).endswith("bot")]
 
-        # format is like: (user_id, username/name,anonyamous or not)
-        mention_users = [
-            (
-                admin[1]
-                if admin[1].startswith("@")
-                else (await mention_html(admin[1], admin[0]))
-            )
-            for admin in user_admins
-            if not admin[2]  # if non-anonyamous admin
-        ]
-        mention_users.sort(key=lambda x: x[1])
+    # format is like: (user_id, username/name,anonyamous or not)
+    mention_users = [
+        (
+            admin[1]
+            if admin[1].startswith("@")
+            else (await mention_html(admin[1], admin[0]))
+        )
+        for admin in user_admins
+        if not admin[2]  # if non-anonyamous admin
+    ]
+    mention_users.sort(key=lambda x: x[1])
 
-        mention_bots = [
-            (
-                admin[1]
-                if admin[1].startswith("@")
-                else (await mention_html(admin[1], admin[0]))
-            )
-            for admin in bot_admins
-        ]
-        mention_bots.sort(key=lambda x: x[1])
+    mention_bots = [
+        (
+            admin[1]
+            if admin[1].startswith("@")
+            else (await mention_html(admin[1], admin[0]))
+        )
+        for admin in bot_admins
+    ]
+    mention_bots.sort(key=lambda x: x[1])
 
-        adminstr += "<b>User Admin te:</b>\n"
-        adminstr += "\n".join(f"- {i}" for i in mention_users)
-        adminstr += "\n\n<b>Bot:</b>\n"
-        adminstr += "\n".join(f"- {i}" for i in mention_bots)
+    adminstr += "<b>User Admin te:</b>\n"
+    adminstr += "\n".join(f"- {i}" for i in mention_users)
+    adminstr += "\n\n<b>Bot:</b>\n"
+    adminstr += "\n".join(f"- {i}" for i in mention_bots)
 
-        await m.reply_text(adminstr + "\n\n" + note)
+    await m.reply_text(adminstr + "\n\n" + note)
 
-    return
+return
