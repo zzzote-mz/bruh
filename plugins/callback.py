487b610f9b5e6753a2bd9910c0c16f765d8d0221
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-23 21:15:23 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -23,3 +23,25 @@ async def close_admin_callback(client, query):
     await query.message.edit_text("Sût leh ani e.")
     await query.answer()
     return
+
+
+
+
+@Client.on_callback_query(filters.regex("^close_i$"))
+async def close_admin(client, query):
+    user_id = query.from_user.id
+    user_mention = query.from_user.mention
+    user_status = (await query.message.chat.get_member(user_id)).status
+    if user_status != "administrator":
+        await query.answer(
+            "Group Admin inih loh chuan iti ve theilo.",
+            show_alert=True,
+        )
+        return
+    await query.message.edit_text(f"{user_mention} hian warning a remove e.")
+    await query.answer()
+    return
+
+
+
+
