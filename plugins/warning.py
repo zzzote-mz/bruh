19d408b7b71ca99d1a324a17d85d91f46784fb63
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-23 20:05:54 +0530

    Update warning.py

--- plugins/warning.py
+++ plugins/warning.py
@@ -21,7 +21,7 @@ async def warn(client, message):
         if xc.status == "administrator":
             await message.reply_text("Admin ka earning theilo")
         zc = message.reply_to_message.from_user.mention
-        await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {zc}\n**➥ID:** {zx}\n**➥A chhan:** {zz}")
+        await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {zc}\n**➥ID:** {zx}\n**➥A chhan:** {zz}", reply_to_message_id=message.reply_to_message.message_id)
         return
     if not message.reply_to_message and len(message.command) == 1:
         await message.reply_text("Command hmang hian i warning duh message reply rawh. I warning chhan dah tel i duh chuan command zawh ah a chhan tur i dah dawn nia")
@@ -39,7 +39,7 @@ async def dwarn(client, message):
         if uvu.status == "administrator":
             await message.reply_text("Admin ka warning theilo")
         umen = message.reply_to_message.from_user.mention
-        await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** {uid}\n**➥A chhan:** Awmlo", reply_to_message_id=message.reply_to_message.message_id)
+        await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** {uid}\n**➥A chhan:** Awmlo")
         await message.reply_to_message.delete()
         await message.delete()
         return
