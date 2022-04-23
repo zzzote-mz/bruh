d0b0221e42a48bd6da2808504531a36aaf44ad3b
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-23 22:26:42 +0530

    Update warning.py

--- plugins/warning.py
+++ plugins/warning.py
@@ -21,20 +21,14 @@ rsrk = InlineKeyboardMarkup(
 async def warn(client, message):
     if message.reply_to_message:
         uid = message.reply_to_message.from_user.id
+        zz = message.text.split(None, 1)[1]
+        if not zz:
+            await message.reply_text("Command zawh ah a chhan dah tel rawh.")
         uvu = await message.chat.get_member(uid)
-        if uvu.status not in {"creator", "administrator"}:
+        if uvu.status in {"creator", "administrator"}:
             await message.reply_text("Admin ka warning theilo.")
         umen = message.reply_to_message.from_user.mention
-        await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** {uid}\n**➥A chhan:** Awmlo", reply_markup=rsrk, reply_to_message_id=message.reply_to_message.message_id)
-        return
-    if len(message.command) == 2:
-        zz = message.text.split(None, 1)[1]
-        zx = message.reply_to_message.from_user.id
-        xc = await message.chat.get_member(zx)
-        if xc.status not in {"creator", "administrator"}:
-            await message.reply_text("Admin ka warning theilo.")
-        zc = message.reply_to_message.from_user.mention
-        await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {zc}\n**➥ID:** {zx}\n**➥A chhan:** {zz}", reply_markup=rsrk, reply_to_message_id=message.reply_to_message.message_id)
+        await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** {uid}\n**➥A chhan:** {zz}", reply_markup=rsrk, reply_to_message_id=message.reply_to_message.message_id)
         return
     if not message.reply_to_message and len(message.command) == 1:
         await message.reply_text("Command hmang hian i warning duh message reply rawh. I warning chhan dah tel i duh chuan command zawh ah a chhan tur i dah dawn nia")
@@ -48,22 +42,14 @@ async def warn(client, message):
 async def dwarn(client, message):
     if message.reply_to_message:
         uid = message.reply_to_message.from_user.id
+        zu = message.text.split(None, 1)[1]
+        if not zu:
+            await message.reply_text("Command zawh ah a chhan dah tel rawh.")
         uvu = await message.chat.get_member(uid)
         if uvu.status not in {"creator", "administrator"}:
             await message.reply_text("Admin ka warning theilo.")
         umen = message.reply_to_message.from_user.mention
-        await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** {uid}\n**➥A chhan:** Awmlo", reply_markup=rsrk)
-        await message.reply_to_message.delete()
-        await message.delete()
-        return
-    if message.reply_to_message and len(message.command) == 2:
-        zz = message.text.split(None, 1)[1]
-        zx = message.reply_to_message.from_user.id
-        xc = await message.chat.get_member(zx)
-        if xc.status not in {"creator", "administrator"}:
-            await message.reply_text("Admin ka warning theilo.")
-        zc = message.reply_to_message.from_user.mention
-        await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {zc}\n**➥ID:** {zx}\n**➥A chhan:** {zz}", reply_markup=rsrk)
+        await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** {uid}\n**➥A chhan:** {zu}", reply_markup=rsrk)
         await message.reply_to_message.delete()
         await message.delete()
         return
