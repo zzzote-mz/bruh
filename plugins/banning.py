cc92508e9879f2bcf7203b94e5dfbc7d64ec2b9a
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-23 20:02:31 +0530

    Update banning.py

--- plugins/banning.py
+++ plugins/banning.py
@@ -22,6 +22,34 @@ async def ban(client, message):
         await client.ban_chat_member(message.chat.id, user_id=idu)
         await client.send_message(message.chat.id, text=f"{umens} hi Ban ani e.", reply_to_message_id=message.message_id)
         return
+    
+    
+    
+@Client.on_message(filters.command("dban", prefixes=["/", "!"]) & filters.group)
+@admins_only
+async def dban(client, message):
+    if not message.reply_to_message and len(message.command) == 1:
+       await message.reply_text("**Command hmang hian i ban duh message reply in emaw, command zawh ah an ID emaw username dah i thawn chauh in mi a ban theih.**")
+       return
+    elif message.reply_to_message:
+        uid = message.reply_to_message.from_user.id
+        umen = message.reply_to_message.from_user.mention
+        await client.ban_chat_member(message.chat.id, user_id=uid)
+        await client.send_message(message.chat.id, text=f"{umen} hi Ban ani e.")
+        await message.reply_to_message.delete()
+        await message.delete()
+        return
+    else:
+        idu = message.text.split(None, 1)[1]
+        hmm = await client.get_users(idu)
+        umens = hmm.mention
+        await client.ban_chat_member(message.chat.id, user_id=idu)
+        await client.send_message(message.chat.id, text=f"{umens} hi Ban ani e.")
+        await message.reply_to_message.delete()
+        await message.delete()
+        return    
+    
+    
       
       
 @Client.on_message(filters.command("unban", prefixes=["/", "!"]) & filters.group)
