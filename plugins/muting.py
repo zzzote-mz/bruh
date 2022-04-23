57aed039deb473ab566372945c6bdb5aeaed8f0b
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-23 20:08:51 +0530

    Update muting.py

--- plugins/muting.py
+++ plugins/muting.py
@@ -23,7 +23,35 @@ async def mute(client, message):
         await client.restrict_chat_member(chat_id=message.chat.id, user_id=idu, permissions=ChatPermissions())
         await client.send_message(message.chat.id, text=f"{umens} hi Mute ani e.", reply_to_message_id=message.message_id)
         return
-      
+ 
+
+
+@Client.on_message(filters.command("dmute", prefixes=["/", "!"]) & filters.group)
+@admins_only
+async def dmute(client, message):
+    if not message.reply_to_message and len(message.command) == 1:
+       await message.reply_text("**Command hmang hian i mute duh message reply in emaw, command zawh ah an ID emaw username dah a i thawn chauh in mi a mute theih.**")
+       return
+    elif message.reply_to_message:
+        uid = message.reply_to_message.from_user.id
+        umen = message.reply_to_message.from_user.mention
+        await client.restrict_chat_member(chat_id=message.chat.id, user_id=uid, permissions=ChatPermissions())
+        await client.send_message(message.chat.id, text=f"{umen} hi Mute ani e.")
+        await message.reply_to_message.delete()
+        await message.delete()
+        return
+    else:
+        idu = message.text.split(None, 1)[1]
+        hmm = await client.get_users(idu)
+        umens = hmm.mention
+        await client.restrict_chat_member(chat_id=message.chat.id, user_id=idu, permissions=ChatPermissions())
+        await client.send_message(message.chat.id, text=f"{umens} hi Mute ani e.")
+        await message.reply_to_message.delete()
+        await message.delete()
+        return
+
+
+
       
 @Client.on_message(filters.command("unmute", prefixes=["/", "!"]) & filters.group)
 @admins_only
