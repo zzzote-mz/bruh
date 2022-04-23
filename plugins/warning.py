b0068c9d4eee7264676ec6edb805bc022a8f9178
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-23 19:19:20 +0530

    Update warning.py

--- plugins/warning.py
+++ plugins/warning.py
@@ -9,20 +9,20 @@ async def warn(client, message):
     if message.reply_to_message:
         uid = message.reply_to_message.from_user.id
         uvu = await client.get_users(uid)
-    elif uvu.status == "administrator":
-        await message.reply_text("Admin ka warning theilo")
+        if uvu.status == "administrator":
+            await message.reply_text("Admin ka warning theilo")
         umen = message.reply_to_message.from_user.mention
         await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** {uid}\n**➥A chhan:** Awmlo", reply_to_message_id=message.reply_to_message.message_id)
         return
-    elif message.reply_to_message and len(message.command) == 2:
+    if message.reply_to_message and len(message.command) == 2:
         zz = message.text.split(None, 1)[1]
         zx = message.reply_to_message.from_user.id
         xc = await client.get_users(zx)
-    elif xc.status == "administrator":
-        await message.reply_text("Admin ka earning theilo")
+        if xc.status == "administrator":
+            await message.reply_text("Admin ka earning theilo")
         zc = message.reply_to_message.from_user.mention
         await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {zc}\n**➥ID:** {zx}\n**➥A chhan:** {zz}")
         return
-    elif not message.reply_to_message and len(message.command) == 1:
+    if not message.reply_to_message and len(message.command) == 1:
         await message.reply_text("Command hmang hian i warning duh message reply rawh. I warning chhan dah tel i duh chuan command zawh ah a chhan tur i dah dawn nia")
         return
