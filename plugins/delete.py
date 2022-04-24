97efc4106dfda51ca8a3cec593386fc574d996bb
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-24 15:10:38 +0530

    Update delete.py

--- plugins/delete.py
+++ plugins/delete.py
@@ -2,13 +2,16 @@ from pyrogram import Client, filters
 from Tereuhte.tetakte.helper import admins_only
 
 
-async def spurge(c: Alita, m: Message):
-    if m.chat.type != "supergroup":
-        await m.reply_text(tlang(m, "purge.err_basic"))
+
+@Client.on_message(filters.command("dltall", prefixes=["/", "!"]) & filters.group)
+@admins_only
+async def spurge(client, message):
+    if message.chat.type != "supergroup":
+        await message.reply_text("Supergroup ami chiah ka delete thei.")
         return
 
-    if m.reply_to_message:
-        message_ids = list(range(m.reply_to_message.message_id, m.message_id))
+    if message.reply_to_message:
+        message_ids = list(range(message.reply_to_message.message_id, message.message_id))
 
         def divide_chunks(l: list, n: int = 100):
             for i in range(0, len(l), n):
@@ -19,22 +22,43 @@ async def spurge(c: Alita, m: Message):
 
         try:
             for plist in m_list:
-                await c.delete_messages(
-                    chat_id=m.chat.id,
+                await client.delete_messages(
+                    chat_id=message.chat.id,
                     message_ids=plist,
                     revoke=True,
                 )
-            await m.delete()
+            await message.delete()
         except MessageDeleteForbidden:
-            await m.reply_text(tlang(m, "purge.old_msg_err"))
+            await message.reply_text("I message delete duh hi ka delete theilo, a chhan chu message hi a hlui tawh ani thei(ni 2 liam tawh message ka delete theilo. Chuan supergroup ami lo chu ka delete theilo, emaw hemi group ah hian message delete theihna permission ka neilo pawh ani thei. A khawi emaw ber vang hian i message delete duh hi ka delete theilo.")
             return
         except RPCError as ef:
-            await m.reply_text(
-                (tlang(m, "general.some_error")).format(
-                    SUPPORT_GROUP=SUPPORT_GROUP,
+            await message.reply_text(
+                   "Message delete theih anilo, harsatna thlen i duh chuan @helptereuhte ah i sawi thei ang.",
                     ef=ef,
-                ),
             )
         return
-    await m.reply_text("Reply to a message to start spurge !")
-    return
+    else:
+        await message.reply_text("Command hmang hian i delete duhna chin a message reply rawh.")
+        return
+    
+    
+    
+    
+    
+@Client.on_message(filters.command("dlt", prefixes=["/", "!"]) & filters.group)
+@admins_only    
+async def dlt(client, message):
+    if message.chat.type != "supergroup":
+        return
+
+    if message.reply_to_message:
+        await message.delete()
+        await client.delete_messages(
+            chat_id=message.chat.id,
+            message_ids=message.reply_to_message.message_id,
+        )
+    else:
+        await message.reply_text("I message delete duh reply rawh.")
+    return   
+    
+    
