fcbcb87b24ae7148ae41d561405c264865847b4a
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-24 16:25:15 +0530

    Update webapp.py

--- plugins/webapp.py
+++ plugins/webapp.py
@@ -1,9 +1,19 @@
 from pyrogram import filters
 from pyrogram import Client 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+rsrp = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Website", web_app="https://www.imrsr.online"
+                    ),
+                ],
+            ]
+        )
 
 
 @Client.on_message(filters.command("rsr", prefixes=["/", "!"]))
 def donate(client, message):
-  client.send_message(message.chat.id, text="A hnuaia **Website** tih button khu hmet la min siamtu website i en thei ang.", reply_to_message_id=message.message_id, disable_web_page_preview=True)
+  client.send_message(message.chat.id, text="A hnuaia **Website** tih button khu hmet la min siamtu website i en thei ang.", reply_markup=rsrp, reply_to_message_id=message.message_id)
   return
