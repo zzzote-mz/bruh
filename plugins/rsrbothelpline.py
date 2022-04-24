4adc82b05b468778c2e93c7e5e270fd1ac5840b5
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-24 17:17:08 +0530

    Update rsrbothelpline.py

--- plugins/rsrbothelpline.py
+++ plugins/rsrbothelpline.py
@@ -36,5 +36,5 @@ rsrke = InlineKeyboardMarkup(
 @Client.on_message(filters.chat(-1001400522942) & filters.new_chat_members)
 async def rsrbothelpline(client, message):
     for rsr in message.new_chat_members:
-          await client.send_message(-1001400522942, text=f"Hello {rsr.mention}\n\n**{message.chat.title}** group ah kan lo lawm a che, he group hi RSR Bot siam a felhlelh awm leh a hman dan zirna hmun ani e, chuan **Rules** tih khu chhiar tur aw, hetah nawi lutuk a rem loh avangin nawi duh tan **Chat Group** tih khu join leh mai tur, **Channel** tih khu Join/Follow ve bawk rawh aw.", reply_markup=rsrke, reply_to_message_id=message.message_id)
+          await client.send_message(-1001400522942, text=f"Hello {rsr.mention}\n\n**{message.chat.title}** group ah kan lo lawm a che, he group hi RSR Bot siam a felhlelh awm leh a hman dan zirna hmun ani e, chuan **Rules** tih khu chhiar tur aw, hetah nawi lutuk a rem loh avangin nawi duh tan **Chat Group** tih khu join leh mai tur, **Channel** tih khu Join/Follow ve bawk rawh aw.", reply_markup=rsrke, reply_to_message_id=message.id)
           return
