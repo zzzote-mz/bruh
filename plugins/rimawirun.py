b7c374a852836521109af0bd9ab1f6f2445e7616
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-24 17:17:34 +0530

    Update rimawirun.py

--- plugins/rimawirun.py
+++ plugins/rimawirun.py
@@ -41,5 +41,5 @@ rsrke = InlineKeyboardMarkup(
 @Client.on_message(filters.chat(-1001167470612) & filters.new_chat_members)
 async def rimawirun(client, message):
     for rsr in message.new_chat_members:
-          await client.send_message(-1001167470612, text=f"Hello {rsr.mention}\n\n**{message.chat.title}** group ah kan lo lawm a che, hemi group a hla download dan i hriat duh chuan a hnuai button a **Tutorial 1** leh **Tutorial 2** tih khu hmet la i en dawn nia, a 2 khan a hman theih ve ve avangin a 2 khan i en dawn nia.\n\nGroup awm nuam le.", reply_markup=rsrke, reply_to_message_id=message.message_id)
+          await client.send_message(-1001167470612, text=f"Hello {rsr.mention}\n\n**{message.chat.title}** group ah kan lo lawm a che, hemi group a hla download dan i hriat duh chuan a hnuai button a **Tutorial 1** leh **Tutorial 2** tih khu hmet la i en dawn nia, a 2 khan a hman theih ve ve avangin a 2 khan i en dawn nia.\n\nGroup awm nuam le.", reply_markup=rsrke, reply_to_message_id=message.id)
           return
