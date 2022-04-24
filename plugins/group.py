b52ad63dfd31a81d7e2b51dba165f9064ee9eb57
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-24 23:29:25 +0530

    Update group.py

--- plugins/group.py
+++ plugins/group.py
@@ -95,9 +95,10 @@ async def report_user(client, message):
     user_mention = reply.from_user.mention if reply.from_user else reply.sender_chat.title
     text = f"{user_mention} message hi Admin hnen ah report ani."
     admin_data = await client.get_chat_members(message.chat.id, filter="administrators")
-    if admin_data.user.is_bot or admin_data.user.is_deleted:
-        continue
-    text += f"[\u2063](tg://user?id={admin.user.id})"
+    for admin in admin_data:
+        if admin.user.is_bot or admin.user.is_deleted:
+            continue
+        text += f"[\u2063](tg://user?id={admin.user.id})"
 
     await message.reply_to_message.reply_text(text)    
     
