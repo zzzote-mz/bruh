45ec51f36ccff65f54aa863a37fd0c5bf1944141
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-24 13:39:47 +0530

    Update thawntir.py

--- plugins/thawntir.py
+++ plugins/thawntir.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from Tereuhte.tetakte.helper import admins_only
 
 
-@Client.on_message(filters.command("tereuhte", prefixes=["/", "!"]) & filters.group)
+@Client.on_message(filters.command("tetakte", prefixes=["/", "!"]) & filters.group)
 @admins_only
 async def thawntir(client, message):
   if not message.reply_to_message and len(message.command) == 1:
