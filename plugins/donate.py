163317e1f2cf96c3c4f4effbfc6b8a51603e72a2
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-24 17:29:39 +0530

    Update donate.py

--- plugins/donate.py
+++ plugins/donate.py
@@ -4,7 +4,7 @@ from pyrogram import Client
 
 
 @Client.on_message(filters.command("donate", prefixes=["/", "!"]))
-def donate(client, message):
-  client.forward_messages(message.chat.id, from_chat_id=-1001640035413, message_ids=30)
-  client.send_message(message.chat.id, text="A chunga Donate tih button hi hmet la min ṭanpui na'n i donate thei ang. Emaw, a hnuaia link hi join la, tah khan donate dan chidang deuh pawh a en theih ang.\n\nhttps://t.me/teamtereuhte", reply_to_message_id=message.message_id, disable_web_page_preview=True)
+async def donate(client, message):
+  await client.forward_messages(message.chat.id, from_chat_id=-1001640035413, message_ids=30)
+  await client.send_message(message.chat.id, text="A chunga Donate tih button hi hmet la min ṭanpui na'n i donate thei ang. Emaw, a hnuaia link hi join la, tah khan donate dan chidang deuh pawh a en theih ang.\n\nhttps://t.me/teamtereuhte", reply_to_message_id=message.id, disable_web_page_preview=True)
   return
