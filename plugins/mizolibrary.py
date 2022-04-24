a06c43df2a59bc29e3823f5a68270fde5114cc1f
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-24 17:25:51 +0530

    Update mizolibrary.py

--- plugins/mizolibrary.py
+++ plugins/mizolibrary.py
@@ -15,5 +15,5 @@ rsrke = InlineKeyboardMarkup(
 @Client.on_message(filters.chat(-1001256541031) & filters.new_chat_members)
 async def mizolibrary(client, message):
     for rsr in message.new_chat_members:
-          await client.send_message(-1001256541031, text=f"Hello {rsr.mention}\n\n**{message.chat.title}** group ah hian kan lo lawm a che. He group hi Story leh a rem rem post na tura siam ania, chutihrual chuan post tur i neih chuan post mawl tawp lovin @rsrmusic hnen ah hian i post tur kha enge ani tih iva hrilh phawt dawn nia, post tawp kan phalloh chhan hi fake leh thil thalo post a awm loh na'n ani e, hun hman nuam le😊", reply_markup=rsrke, reply_to_message_id=message.message_id)
+          await client.send_message(-1001256541031, text=f"Hello {rsr.mention}\n\n**{message.chat.title}** group ah hian kan lo lawm a che. He group hi Story leh a rem rem post na tura siam ania, chutihrual chuan post tur i neih chuan post mawl tawp lovin @rsrmusic hnen ah hian i post tur kha enge ani tih iva hrilh phawt dawn nia, post tawp kan phalloh chhan hi fake leh thil thalo post a awm loh na'n ani e, hun hman nuam le😊", reply_markup=rsrke, reply_to_message_id=message.id)
           return
