f8a5fadd992015d3d93089020f90eb572c5f207c
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-24 17:27:11 +0530

    Update locker.py

--- plugins/locker.py
+++ plugins/locker.py
@@ -7,7 +7,7 @@ from pyrogram.types import ChatPermissions
 @admins_only
 async def close(client, message):
     await client.set_chat_permissions(chat_id=message.chat.id, permissions=ChatPermissions())
-    await client.send_message(message.chat.id, text="**Group ah hian member tan message thawn theih loh tura siam ani e.**", reply_to_message_id=message.message_id)
+    await client.send_message(message.chat.id, text="**Group ah hian member tan message thawn theih loh tura siam ani e.**", reply_to_message_id=message.id)
     return
       
 @Client.on_message(filters.command("open", prefixes=["/", "!"]) & filters.group)
@@ -24,6 +24,6 @@ async def open(client, message):
             can_send_polls=True
         )
     )
-    await client.send_message(message.chat.id, text="**Group ah hian member tan paw'n message thawn theih tura siam ani e.**", reply_to_message_id=message.message_id)
+    await client.send_message(message.chat.id, text="**Group ah hian member tan paw'n message thawn theih tura siam ani e.**", reply_to_message_id=message.id)
     return     
       
