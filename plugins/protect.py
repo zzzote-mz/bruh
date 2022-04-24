bee6ddacd6f122b28d43910bfa45ad64bf9d2fb6
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-24 11:29:44 +0530

    Create protect.py

---
+++ plugins/protect.py
@@ -0,0 +1,29 @@
+#©️ 2022 RSR
+
+from pyrogram import Client, filters
+from Tereuhte.tetakte.helper import admins_only
+
+
+@Client.on_message(filters.command("protect", prefixes=["/", "!"]) & filters.group)
+@admins_only
+async def protect(client, message):
+  if message.reply_to_message:
+     await client.copy_message(
+       chat_id=message.chat.id,
+       from_chat_id= message.chat.id,
+       message_id=message.reply_to_message.message_id,
+       protect_content=True
+     )
+     await client.delete_messages(
+       message.chat.id,
+       message.reply_to_message.message_id
+     )
+     await message.delete()
+  else:
+       await client.send_message(
+         message.chat.id,
+         text="**Message reply rawh.**",
+         reply_to_message_id=message.message_id
+       )
+  
+     
