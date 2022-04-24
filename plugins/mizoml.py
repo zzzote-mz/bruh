391d4ae7e3811e60a99ecf152f643b11f0f587bd
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-04-24 17:25:27 +0530

    Update mizoml.py

--- plugins/mizoml.py
+++ plugins/mizoml.py
@@ -15,5 +15,5 @@ rsrke = InlineKeyboardMarkup(
 @Client.on_message(filters.chat(-1001625845943) & filters.new_chat_members)
 async def mizoml(client, message):
     for rsr in message.new_chat_members:
-          await client.send_video(-1001625845943, video="https://telegra.ph/file/d53ad20d816189453d751.mp4", caption=f"Hello {rsr.mention}\n\n**{message.chat.title}** group ah hian kan lo lawm a che😘", reply_markup=rsrke, reply_to_message_id=message.message_id)
+          await client.send_video(-1001625845943, video="https://telegra.ph/file/d53ad20d816189453d751.mp4", caption=f"Hello {rsr.mention}\n\n**{message.chat.title}** group ah hian kan lo lawm a che😘", reply_markup=rsrke, reply_to_message_id=message.id)
           return
