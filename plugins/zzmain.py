57c67e4ce6b1bde55d6cd9dd5c3929928b427cbf
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-05-28 19:47:16 +0530

    Update zzmain.py

--- plugins/zzmain.py
+++ plugins/zzmain.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 @Client.on_message(filters.command("start", prefixes=["/", "!"]) & filters.private)
 async def gstart(client, message):
-    await client.send_video(message.chat.id, video="https://telegra.ph/file/96b84705d3c1cfc51b0aa.mp4", reply_to_message_id=message.id, protect_content=True)
+    await client.send_video(message.chat.id, video="https://telegra.ph/file/0a254c1f7493d10d17d6b.mp4", reply_to_message_id=message.id, protect_content=True)
     await asyncio.sleep(3)
     await client.send_message(message.chat.id, text="Hello, Bot hi siam lai mek ania, ala hman theih rihloh.")
     return
