1a1f7dbd3c0d9305f1b38f9ac80396485b415668
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-05-30 20:26:01 +0530

    Update warning.py

--- plugins/warning.py
+++ plugins/warning.py
@@ -1,6 +1,6 @@
 # ©️2022 RSR
 from pyrogram import Client, filters
-from Tereuhte.tetakte.helper import admins_only
+from Tereuhte.tetakte.admins import admin_status
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
@@ -19,14 +19,22 @@ rsrk = InlineKeyboardMarkup(
 @Client.on_message(filters.command("warn", prefixes=["/", "!"]) & filters.group)
 @admins_only
 async def warn(client, message):
+    heh = message.from_user.id
+    huh = await message.chat.get_member(heh)
+    if not huh.status in admin_status:
+        return await message.reply_text(
+            "Admin i nih loh chuan i ti ve theilo."
+        )
     if len(message.command) < 2:
         return await message.reply_text("Command zawh ah a chhan dah tel rawh.")
     if message.reply_to_message:
         uid = message.reply_to_message.from_user.id
         zz = message.text.split(None, 1)[1]
-        uvu = await client.get_chat_member(message.chat.id, uid)
-        if uvu.status in {"creator", "administrator"}:
-            return await message.reply_text("Admin ka warning theilo.")
+        lal = await message.chat.get_member(uid)
+        if lal.status in admin_status:
+            return await message.reply_text(
+                "Admin chu ka ban theilo."
+            )
         umen = message.reply_to_message.from_user.mention
         await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** {uid}\n**➥A chhan:** {zz}", reply_markup=rsrk, reply_to_message_id=message.reply_to_message.id)
         return
@@ -40,14 +48,22 @@ async def warn(client, message):
 @Client.on_message(filters.command("dwarn", prefixes=["/", "!"]) & filters.group)
 @admins_only
 async def dwarn(client, message):
+    heh = message.from_user.id
+    huh = await message.chat.get_member(heh)
+    if not huh.status in admin_status:
+        return await message.reply_text(
+            "Admin i nih loh chuan i ti ve theilo."
+        )
     if len(message.command) < 2:
         return await message.reply_text("Command zawh ah a chhan dah tel rawh.")
     if message.reply_to_message:
         uid = message.reply_to_message.from_user.id
         zu = message.text.split(None, 1)[1]
-        uvu = await client.get_chat_member(message.chat.id, uid)
-        if uvu.status in {"creator", "administrator"}:
-            await message.reply_text("Admin ka warning theilo.")
+        lal = await message.chat.get_member(uid)
+        if lal.status in admin_status:
+            return await message.reply_text(
+                "Admin chu ka ban theilo."
+            )
         umen = message.reply_to_message.from_user.mention
         await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** {uid}\n**➥A chhan:** {zu}", reply_markup=rsrk)
         await message.reply_to_message.delete()
