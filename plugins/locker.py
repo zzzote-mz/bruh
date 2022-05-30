79851554a398a037b0fb01443ea56d74851202e9
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-05-30 14:17:49 +0530

    Update locker.py

--- plugins/locker.py
+++ plugins/locker.py
@@ -1,18 +1,28 @@
 # ©️2022 RSR
 from pyrogram import Client, filters
-from Tereuhte.tetakte.helper import admins_only
+from Tereuhte.tetakte.admins import admin_status
 from pyrogram.types import ChatPermissions
 
 @Client.on_message(filters.command("close", prefixes=["/", "!"]) & filters.group)
-@admins_only
 async def close(client, message):
+    heh = message.from_user.id
+    huh = await message.chat.get_member(heh)
+    if not huh.status in admin_status:
+        return await message.reply_text(
+            "Admin i nih loh chuan i ti ve theilo."
+        )
     await client.set_chat_permissions(chat_id=message.chat.id, permissions=ChatPermissions())
     await client.send_message(message.chat.id, text="**Group ah hian member tan message thawn theih loh tura siam ani e.**", reply_to_message_id=message.id)
     return
       
 @Client.on_message(filters.command("open", prefixes=["/", "!"]) & filters.group)
-@admins_only
 async def open(client, message):
+    heh = message.from_user.id
+    huh = await message.chat.get_member(heh)
+    if not huh.status in admin_status:
+        return await message.reply_text(
+            "Admin i nih loh chuan i ti ve theilo."
+        )
     await client.set_chat_permissions(
         chat_id=message.chat.id,
         permissions=ChatPermissions(
