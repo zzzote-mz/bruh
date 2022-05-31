82cdb9d7aa3c5cefad42768f016f984110c9bfd0
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-05-31 18:53:51 +0530

    Update warning.py

--- plugins/warning.py
+++ plugins/warning.py
@@ -2,6 +2,12 @@
 from pyrogram import Client, filters
 from Tereuhte.tetakte.admins import admin_status
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import (
+    ChatAdminRequired,
+    PeerIdInvalid,
+    RightForbidden,
+    UserNotParticipant,
+)
 
 
 
@@ -27,20 +33,29 @@ async def warn(client, message):
     if message.reply_to_message and len(message.command) < 2:
         return await message.reply_text("Command zawh ah a chhan dah tel rawh.")
     if message.reply_to_message:
-        uid = message.reply_to_message.from_user.id
-        zz = message.text.split(None, 1)[1]
-        lal = await message.chat.get_member(uid)
-        if lal.status in admin_status:
-            return await message.reply_text(
-                "Admin chu ka warning theilo."
-            )
-        if uid == 1060318977:
-            return await message.reply_text(
-                "A ni hi chu min siamtu a ni a, chuvang chuan ka warning ve theilo."
-            )
-        umen = message.reply_to_message.from_user.mention
-        await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** `{uid}`\n**➥A chhan:** {zz}", reply_markup=rsrk, reply_to_message_id=message.reply_to_message.id)
-        return
+        try:
+            uid = message.reply_to_message.from_user.id
+            zz = message.text.split(None, 1)[1]
+            lal = await message.chat.get_member(uid)
+            if lal.status in admin_status:
+                return await message.reply_text(
+                    "Admin chu ka warning theilo."
+                )
+            if uid == 1060318977:
+                return await message.reply_text(
+                    "A ni hi chu min siamtu a ni a, chuvang chuan ka warning ve theilo."
+                )
+            umen = message.reply_to_message.from_user.mention
+            await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** `{uid}`\n**➥A chhan:** {zz}", reply_markup=rsrk, reply_to_message_id=message.reply_to_message.id)
+            return
+        except ChatAdminRequired:
+            return await message.reply_text("Admin ka nilo a chuvang chuan tumah ka warning theilo.")
+        except PeerIdInvalid:
+            return await message.reply_text("Warning tur ID hi ka hmulo.")
+        except RightForbidden:
+            return await message.reply_text("Hetah mi warning theihna permission ka neilo.")
+        except UserNotParticipant:
+            return await message.reply_text("Warning tur hi group ah hian a awmlo.")
     else:
         await message.reply_text("Command hmang hian i warning duh message reply rawh.")
         return
@@ -60,22 +75,31 @@ async def dwarn(client, message):
     if message.reply_to_message and len(message.command) < 2:
         return await message.reply_text("Command zawh ah a chhan dah tel rawh.")
     if message.reply_to_message:
-        uid = message.reply_to_message.from_user.id
-        zu = message.text.split(None, 1)[1]
-        lal = await message.chat.get_member(uid)
-        if lal.status in admin_status:
-            return await message.reply_text(
-                "Admin chu ka warning theilo."
-            )
-        if uid == 1060318977:
-            return await message.reply_text(
-                "A ni hi chu min siamtu a ni a, chuvang chuan ka warning ve theilo."
-            )
-        umen = message.reply_to_message.from_user.mention
-        await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** `{uid}`\n**➥A chhan:** {zu}", reply_markup=rsrk)
-        await message.reply_to_message.delete()
-        await message.delete()
-        return
+        try:
+            uid = message.reply_to_message.from_user.id
+            zu = message.text.split(None, 1)[1]
+            lal = await message.chat.get_member(uid)
+            if lal.status in admin_status:
+                return await message.reply_text(
+                    "Admin chu ka warning theilo."
+                )
+            if uid == 1060318977:
+                return await message.reply_text(
+                    "A ni hi chu min siamtu a ni a, chuvang chuan ka warning ve theilo."
+                )
+            umen = message.reply_to_message.from_user.mention
+            await client.send_message(message.chat.id, text=f"**❗ Warning**\n\n**➥User:** {umen}\n**➥ID:** `{uid}`\n**➥A chhan:** {zu}", reply_markup=rsrk)
+            await message.reply_to_message.delete()
+            await message.delete()
+            return
+        except ChatAdminRequired:
+            return await message.reply_text("Admin ka nilo a chuvang chuan tumah ka warning theilo.")
+        except PeerIdInvalid:
+            return await message.reply_text("Warning tur ID hi ka hmulo.")
+        except RightForbidden:
+            return await message.reply_text("Hetah mi warning theihna permission ka neilo.")
+        except UserNotParticipant:
+            return await message.reply_text("Warning tur hi group ah hian a awmlo.")
     else:
         await message.reply_text("Command hmang hian i warning duh message reply rawh.")
         return
