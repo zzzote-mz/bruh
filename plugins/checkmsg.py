27a142e387a282116944250782bf87790b89277e
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-06-01 23:18:56 +0530

    Create checkmsg.py

---
+++ plugins/checkmsg.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, filters
+
+
+
+@Client.on_message(filters.group & filters.command("msg", prefixes=["/", "!"]))
+async def msg(client, message):
+    if message.reply_to_message:
+        await client.send_message(
+            message.chat.id,
+            text=f"He Group ah hian message `{message.message_id}` thawn ani tawh.",
+            reply_to_message_id=message.reply_to_message.id
+        )
+    else:
+        await client.send_message(
+            message.chat.id,
+            text=f"He Group ah hian message `{message.message_id}` thawn ani tawh.",
+            reply_to_message_id=message.id
+        )
+        
