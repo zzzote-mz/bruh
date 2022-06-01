2b3336851c3a48b0626d0e8b9a5cdd19832fc94f
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-06-01 23:09:59 +0530

    Create channelid.py

---
+++ plugins/channelid.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+
+
+@pbot.on_message(filters.command("chid", prefixes=["/", "!"]))
+async def channelid(client, message):
+    if not message.reply_to_message:
+        await client.send_message(
+          message.chat.id,
+          text="Channel message rawn forward la, chu chu command hian reply rawh",
+          reply_to_message_id=message.id
+        )
+    else:
+        test = f"**{message.reply_to_message.forward_from_chat.title} ID:** `{message.reply_to_message.forward_from_chat.id}`"
+        await client.send_message(
+          message.chat.id,
+          text=test,
+          reply_to_message_id=message.id
+        )
