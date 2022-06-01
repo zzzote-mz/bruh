16de3c2d6a5f07a2a2ba8600d4e746d680293827
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-06-02 00:04:24 +0530

    Update helper.py

--- Tereuhte/tetakte/helper.py
+++ Tereuhte/tetakte/helper.py
@@ -1,4 +1,4 @@
-from typing import Callable, Coroutine, List, Union, Dict
+from typing import Callable, Coroutine, List, Union, Dict, Tuple
 from pyrogram.types import Chat, Message, User
 from pyrogram import Client
 from functools import wraps
@@ -52,3 +52,21 @@ def admins_only(func: Callable) -> Coroutine:
             return
 
     return wrapper
+
+
+
+
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    """ run command in terminal """
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+
