009becac7d0607244f159764b6b702aea3938abd
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-06-01 21:51:32 +0530

    Update identity.py

--- plugins/identity.py
+++ plugins/identity.py
@@ -6,32 +6,6 @@ from Tereuhte.tetakte.admins import group_types
 
 @Client.on_message(filters.command("id", prefixes=["/", "!"]))
 async def identity(client, message):
-    if message.reply_to_message and message.reply_to_message.forward_from:
-        user1 = message.reply_to_message.from_user
-        user2 = message.reply_to_message.forward_from
-        await client.send_message(
-          message.chat.id,
-          text=f"**{user1.mention} ID:** `{user1.id}`\n**{user2.mention} ID:** `{user2.id}`",
-          reply_to_message_id=message.id
-        )
-        return
-    if not message.reply_to_message and message.chat.type in group_types:
-        chan = message.chat
-        user = message.from_user
-        await client.send_message(
-          message.chat.id,
-          text=f"**{user.mention} ID:** `{user.id}`\n**Group ID:** `{chan.id}`",
-          reply_to_message_id=message.id
-        )
-        return
-    if not message.reply_to_message and message.chat.type == enums.ChatType.PRIVATE:
-         user = message.from_user
-         await client.send_message(
-           message.chat.id,
-           text=f"**{user.mention} ID:** `{user.id}`",
-           reply_to_message_id=message.id
-         )
-         return
     if len(message.command) == 2:
          userq = message.text.split(None, 1)[1]
          userqs = await client.get_users(userq)
@@ -41,3 +15,39 @@ async def identity(client, message):
            reply_to_message_id=message.id
          )
          return
+    elif message.reply_to_message:
+        user1 = message.from_user
+        user2 = message.reply_to_message.from_user
+        await client.send_message(
+          message.chat.id,
+          text=f"**{user1.mention} ID:** `{user1.id}`\n**{user2.mention} ID:** `{user2.id}`",
+          reply_to_message_id=message.id
+        )
+        return
+    elif message.reply_to_message and message.reply_to_message.forward_from:
+        user1 = message.reply_to_message.from_user
+        user2 = message.reply_to_message.forward_from
+        await client.send_message(
+          message.chat.id,
+          text=f"**{user1.mention} ID:** `{user1.id}`\n**{user2.mention} ID:** `{user2.id}`",
+          reply_to_message_id=message.id
+        )
+        return
+    elif not message.reply_to_message and message.chat.type in group_types:
+        chan = message.chat
+        user = message.from_user
+        await client.send_message(
+          message.chat.id,
+          text=f"**{user.mention} ID:** `{user.id}`\n**Group ID:** `{chan.id}`",
+          reply_to_message_id=message.id
+        )
+        return
+    elif not message.reply_to_message and message.chat.type == enums.ChatType.PRIVATE:
+         user = message.from_user
+         await client.send_message(
+           message.chat.id,
+           text=f"**{user.mention} ID:** `{user.id}`",
+           reply_to_message_id=message.id
+         )
+         return
+    
