8e299042952916bcf94b5c8228ba492d8bf27814
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-06-01 23:23:50 +0530

    Update checkmsg.py

--- plugins/checkmsg.py
+++ plugins/checkmsg.py
@@ -7,13 +7,13 @@ async def msg(client, message):
     if message.reply_to_message:
         await client.send_message(
             message.chat.id,
-            text=f"He Group ah hian message `{message.message_id}` thawn ani tawh.",
+            text=f"He Group ah hian message `{message.id}` thawn ani tawh.",
             reply_to_message_id=message.reply_to_message.id
         )
     else:
         await client.send_message(
             message.chat.id,
-            text=f"He Group ah hian message `{message.message_id}` thawn ani tawh.",
+            text=f"He Group ah hian message `{message.id}` thawn ani tawh.",
             reply_to_message_id=message.id
         )
         
